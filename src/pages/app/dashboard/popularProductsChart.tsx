e25db72ae18e3d94c6730f033ce0390669fe0ee2
thiago <thiagotsdelima@gmail.com>
2024-04-26 14:08:40 -0300

    update linux

--- src/pages/app/dashboard/popularProductsChart.tsx
+++ src/pages/app/dashboard/popularProductsChart.tsx
@@ -1,5 +1,6 @@
 import { BarChart } from 'lucide-react'
-import { Pie, PieChart, ResponsiveContainer } from 'recharts'
+import { Cell, Pie, PieChart, ResponsiveContainer } from 'recharts'
+import colors from 'tailwindcss/colors'
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
@@ -9,6 +10,14 @@ const data = [
   { product: 'Marguerita', amount: 12 },
   { product: 'four cheese', amount: 67 },
   { product: 'Frango', amount: 45 },
+]
+
+const COLORS = [
+  colors.sky[500],
+  colors.amber[500],
+  colors.violet[500],
+  colors.emerald[500],
+  colors.rose[500],
 ]
 
 export function PopularProductsChart() {
@@ -25,7 +34,56 @@ export function PopularProductsChart() {
       <CardContent>
         <ResponsiveContainer width="100%" height={240}>
           <PieChart style={{ fontSize: 12 }}>
-            <Pie data={data} dataKey="amount" nameKey="product" />
+            <Pie
+              data={data}
+              dataKey="amount"
+              nameKey="product"
+              cx="50%"
+              cy="50%"
+              outerRadius={86}
+              innerRadius={64}
+              strokeWidth={8}
+              labelLine={false}
+              label={({
+                cx,
+                cy,
+                midAngle,
+                innerRadius,
+                outerRadius,
+                value,
+                index,
+              }) => {
+                const RADIAN = Math.PI / 180
+                const radius = 12 + innerRadius + (outerRadius - innerRadius)
+                const x = cx + radius * Math.cos(-midAngle * RADIAN)
+                const y = cy + radius * Math.sin(-midAngle * RADIAN)
+
+                return (
+                  <text
+                    x={x}
+                    y={y}
+                    className="fill-muted-foreground text-xs"
+                    textAnchor={x > cx ? 'start' : 'end'}
+                    dominantBaseline="central"
+                  >
+                    {data[index].product.length > 12
+                      ? data[index].product.substring(0, 12).concat('...')
+                      : data[index].product}{' '}
+                    ({value})
+                  </text>
+                )
+              }}
+            >
+              {data.map((_, index) => {
+                return (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index]}
+                    className="stroke-background hover:opacity-80"
+                  />
+                )
+              })}
+            </Pie>
           </PieChart>
         </ResponsiveContainer>
       </CardContent>
